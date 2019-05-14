060444237f5aee1de3bbeb79e7f3ea02664b6456
zheng.zh <zheng.zh@belle.com.cn>
2019-05-14 10:28:26 +0800

    添加权限组件

---
+++ template/packages/components/ControlAndData.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="belle-controlanddata-container" @click="onclick()">
+    <slot name="button"  v-if="isShow"/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ControlAndData',
+  props: {
+    id: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      isShow: true
+    }
+  },
+  mounted () {
+    const path = this.$router.options.routes[0].path
+    let rule = localStorage.getItem('rule')
+    const ruleobj = JSON.parse(rule)
+    this.isShow = this.id ? !!ruleobj[path][this.id] : true
+  },
+  methods: {
+    onclick () {
+      // window._hmt.push(['_trackEvent', 'music', 'play', 'Hey Jude'])
+    }
+  }
+}
+</script>
+<style>
+
+</style>
