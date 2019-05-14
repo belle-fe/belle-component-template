521dfa3b97d9ce61b0177936b561c8db7a94e611
zheng.zh <zheng.zh@belle.com.cn>
2019-05-14 11:20:41 +0800

    去除分号报错

--- template/packages/index.js
+++ template/packages/index.js
@@ -1,13 +1,13 @@
 // 导入组件，组件必须声明 name
-import BelleComponent from './src';
+import BelleComponent from './src'
 
 // 为组件提供 install 安装方法，供按需引入
 BelleComponent.install = function(Vue) {
-  Vue.component(BelleComponent.name, BelleComponent);
-};
+  Vue.component(BelleComponent.name, BelleComponent)
+}
 
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
+  BelleComponent.install(window.Vue)
 }
 // 默认导出组件
-export default BelleComponent;
+export default BelleComponent
