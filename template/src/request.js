3d968dcbf55b0ae7343f90fafce52ed2a5f2bb4a
wu.h <332418288@qq.com>
2019-05-31 13:56:42 +0800

    axios

---
+++ template/src/request.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import { Message } from 'element-ui' // MessageBox
+
+// 创建axios实例
+const service = axios.create({
+  baseURL: 'http://10.0.30.96/mock/149', // api 的 base_url
+  timeout: 5000 // 请求超时时间
+})
+
+// request拦截器
+service.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    // Do something with request error
+    console.log(error) // for debug
+    Promise.reject(error)
+  }
+)
+
+// response 拦截器
+service.interceptors.response.use(
+  response => {
+    return response.data
+  },
+  error => {
+    console.log('err' + error) // for debug
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service
