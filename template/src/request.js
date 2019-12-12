08a282f73745b1a66fffac50fa4eccd811efcd52
zheng.zh <zheng.zh@belle.com.cn>
2019-12-12 17:24:42 +0800

    修改请求组件

--- template/src/request.js
+++ template/src/request.js
@@ -3,7 +3,7 @@ import { Message } from 'element-ui' // MessageBox
 
 // 创建axios实例
 const service = axios.create({
-  baseURL: 'http://10.0.30.96/mock/149', // api 的 base_url
+  baseURL: '', // api 的 base_url
   timeout: 5000 // 请求超时时间
 })
 
